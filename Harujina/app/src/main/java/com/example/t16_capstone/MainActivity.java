bf8ad813d1353caa402e31dc4db22a53b00877b6
ykmxxi <yunkang97@naver.com>
2022-05-23 11:45:29 +0900

    0523 알림 기능 구현

--- Harujina/app/src/main/java/com/example/t16_capstone/MainActivity.java
+++ Harujina/app/src/main/java/com/example/t16_capstone/MainActivity.java
@@ -80,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
         settingBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), AlarmActivity.class);
+                Intent intent = new Intent(getApplicationContext(), SettingDialog.class);
                 startActivity(intent);
             }
         });

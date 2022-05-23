1735dd4b711419ba2a403df602bf277c519e9174
ykmxxi <yunkang97@naver.com>
2022-05-23 10:50:49 +0900

    0523 권한 허용, 알람 기능 구현

---
+++ Harujina/app/src/main/java/com/example/t16_capstone/CommunicationBinding.java
@@ -0,0 +1,111 @@
+package com.example.t16_capstone;
+
+import android.app.Activity;
+import android.database.Cursor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class StoryContents {
+    // StoryContentsDB, RecommendedDB, 상담권유내용 들을 각각 받기위한 클래스입니다.
+    private ArrayList<String> contents;
+    private ArrayList<Integer> viewStates;
+    private ArrayList<Integer> imageStates;
+
+    StoryContents() {
+        contents = new ArrayList();
+        viewStates = new ArrayList();
+        imageStates = new ArrayList();
+    }
+
+    public void addStory(String s, int views, int images) {
+        contents.add(s);
+        viewStates.add(views);
+        imageStates.add(images);
+    }
+
+    public String[] getContents() {
+        String[] contentStrs = new String[contents.size()];
+        for(int i =0; i<viewStates.size(); i++)
+            contentStrs[i] = contents.get(i);
+        return contentStrs;
+    }
+    public int[] getViewStates() {
+        int[] views = new int[viewStates.size()];
+        for(int i =0; i<viewStates.size(); i++)
+            views[i] = viewStates.get(i);
+        return views;
+    }
+    public int[] getImageStates() {
+        int[] images = new int[imageStates.size()];
+        for(int i =0; i<imageStates.size(); i++)
+            images[i] = imageStates.get(i);
+        return images;
+    }
+}
+
+public class CommunicationBinding {
+    private Activity menu;
+    private String emotionResult;
+    private StoryContents DBStory;
+    private Cursor[] cursors;
+    private Cursor storyCursor, recommendCursor;
+    private DatabaseService dbsvs;
+
+    String[] recommendCounseling = {"끝내기전 저 긴히 할말이 있어요.", "최근에 친구님이 많이 힘드셨다는 걸 느껴요...", "힘드시다는 것이 말할 수 없는 비밀이시라면 숨기셔두돼요.",
+            "그래도 말해서 나아지는 일이 간혹 있더라구요.", "그것이 적어도 나 자신이어도 좋구요..!", "아니면 혹시 말하고 싶은 곳을 찾고 거라면...",
+            "상담사를 찾아가 보는 것도 권해요.", "물론 상담사도 사람이니 꺼려질 수도 있고, 맞지 않을수도 있어요.", "어찌됐건 사람들은 서로 공유하고 싶은 사람을 찾는 거겠죠.",
+            "그것이 고통이라도요.", "만약 상담에 가신다면, 일기장을 보여주는 것도 도움이 될거에요.", "나아질 미래를 응원할게요.", "그리고 언제나 자신을 믿을 수 있길 바래요."};
+    int[] recommendCounselingImages = {0, 7, 5, 0, 5, 7, 0, 0, 0, 5, 0, 5, 0};
+
+    public CommunicationBinding(Activity menu, String emotionResult) {
+        this.menu = menu;
+        this.emotionResult = emotionResult;
+
+        dbsvs = new DatabaseService(menu);
+        DBStory = new StoryContents();
+
+        cursors = dbsvs.getStoryByEmotionAndRecommend(emotionResult);
+        storyCursor = cursors[0];
+        recommendCursor = cursors[1];
+    }
+
+    // StoryContents객체에 add를 통해 내용 구분없이 쭉 붙인다.
+    public StoryContents readStoryContents() {
+        // 기본 상호작용
+        while (storyCursor.moveToNext()){
+            DBStory.addStory(storyCursor.getString(2), storyCursor.getInt(3), storyCursor.getInt(4));
+        }
+        storyCursor.close();
+        // 추천 상호작용
+        if(recommendCursor == null) {
+            // skip
+        } else {
+            while (recommendCursor.moveToNext()){
+                // 전처리 포함
+                String[] temp = recommendCursor.getString(2).split("#");
+                temp[1] = temp[1] + "(이)라는 " + temp[0] + "에요.";
+                temp[0] = "마지막으로 " + temp[0] + "하나 추천드리고 싶네요.";
+                temp[2] = temp[2];
+                DBStory.addStory(temp[0], 0, 0);
+                DBStory.addStory(temp[1], 0, 0);
+                DBStory.addStory(temp[2], 0, 0);
+                DBStory.addStory("꼭 확인해 보셨음 하네요.", 0, 0);
+            }
+            recommendCursor.close();
+        }
+        // 상담권유 상호작용
+        if(checkContinuousEmotion()) {
+            for(int i =0; i<recommendCounseling.length; i++)
+                DBStory.addStory(recommendCounseling[i], 0, recommendCounselingImages[i]);
+        } else {
+            // skip
+        }
+        return DBStory;
+    }
+
+    private boolean checkContinuousEmotion() {
+        // 부정적 감정이 5일 이상 지속되는지 확인.
+        return false;
+    }
+}
---
+++ Harujina/app/src/main/java/com/example/t16_capstone/MainActivity.java
@@ -0,0 +1,147 @@
+package com.example.t16_capstone;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int REQUEST_PERMISSION = 11;
+
+    private BackKeyHandler backKeyHandler = new BackKeyHandler(this);
+    private final int MY_PERMISSIONS_REQUEST_CAMERA = 1001;
+    private ImageView characterMain;
+    private ImageView speechBubbleMain;
+
+    // 데이터베이스 초기화
+    private DatabaseService dbsvs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // db 첫 초기화
+        DatabaseService dbsvs = new DatabaseService(this);
+
+        // 권한 엑세스 함수
+        checkPermission();
+
+        // 애니메이션
+        characterMain = (ImageView) findViewById(R.id.characterMain);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.floating);
+        characterMain.startAnimation(animation);
+
+        speechBubbleMain = findViewById(R.id.speechBubbleMain);
+        AnimationDrawable animationDrawable = (AnimationDrawable) speechBubbleMain.getBackground();
+        animationDrawable.start();
+
+        ImageButton exitBtn = (ImageButton) findViewById(R.id.exitBtn);
+        exitBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AnalysisMenu.class);
+                intent.putExtra("Argv", "MainToDesc");
+                // 화면전환 애니메이션 제거
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+                finish(); //액티비티 종료
+            }
+        });
+
+        ImageButton nextBtn = (ImageButton) findViewById(R.id.nextBtn);
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AnalysisMenu.class);
+                intent.putExtra("Argv", "MainToDesc");
+                // 화면전환 애니메이션 제거
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+                finish(); //액티비티 종료
+            }
+        });
+
+        ImageButton settingBtn = (ImageButton) findViewById(R.id.settingBtn);
+        settingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AlarmActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    //권한 요청
+    public void checkPermission() {
+        int permissionCamera = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA); // 카메라 권한
+        int permissionRead = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE); // 파일 읽어오기 권한
+        int permissionWrite = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE); // 파일 저장 권한
+
+        // 3가지 권한 중 한개라도 없으면 권한 요청 실행
+        if (permissionCamera != PackageManager.PERMISSION_GRANTED
+                || permissionRead != PackageManager.PERMISSION_GRANTED
+                || permissionWrite != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                Toast.makeText(this, "이 앱을 실행하기 위해 권한이 필요합니다.", Toast.LENGTH_SHORT).show();
+            }
+
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_PERMISSION);
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_PERMISSION: {
+                // 권한이 취소되면 result 배열은 비어있다.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Toast.makeText(this, "권한 확인", Toast.LENGTH_LONG).show();
+
+                } else {
+                    Toast.makeText(this, "권한 없음", Toast.LENGTH_LONG).show();
+                    finish(); //권한이 없으면 앱 종료
+                }
+            }
+        }
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        backKeyHandler.onBackPressed();
+    }
+
+    /*
+    public void onClick(View view) {
+        // 화면 전환
+        Intent intent = new Intent(this, AnalysisMenu.class);
+        // 갈 화면에 줄 인자를 설정한다.
+        intent.putExtra("Argv", "MainToDesc");
+        // 화면전환 애니메이션 제거
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        startActivity(intent);
+        finish(); //액티비티 종료
+    }
+    */
+
+}
